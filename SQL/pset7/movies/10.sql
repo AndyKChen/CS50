7d4f0a8a32e9b9632c5d5902a36120918e31603f
AndyKChen <41531111+AndyKChen@users.noreply.github.com>
2020-05-22 19:17:32 -0400

    Delete 10.sql

--- SQL/pset7/movies/10.sql
+++
@@ -1,6 +0,0 @@
-SELECT people.name
-FROM people
-INNER JOIN directors ON people.id = directors.person_id
-INNER JOIN ratings ON movies.id = ratings.movie_id
-INNER JOIN movies ON directors.movie_id = movies.id
-WHERE ratings.rating >= 9
