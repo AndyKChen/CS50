8e4403cc131bf48955146aed975b883df5482a74
AndyKChen <41531111+AndyKChen@users.noreply.github.com>
2020-05-22 19:17:38 -0400

    Delete 11.sql

--- SQL/pset7/movies/11.sql
+++
@@ -1,8 +0,0 @@
-SELECT movies.title
-FROM movies
-INNER JOIN ratings ON movies.id = ratings.movie_id
-INNER JOIN stars ON movies.id = stars.movie_id
-INNER JOIN people ON stars.person_id = people.id
-WHERE people.name = "Chadwick Boseman"
-ORDER BY ratings.rating DESC
-LIMIT 5;
